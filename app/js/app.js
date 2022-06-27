f2673247c840e4340d664e84e17d234a4b431748
MaximProger <maksimkoshman090@gmail.com>
2022-06-27 12:46:01 +0300

    Добавил остальные блоки

--- app/js/app.js
+++ app/js/app.js
@@ -164,20 +164,68 @@ $(document).ready(function () {
     billSectionHidden.toggle();
   });
 
-  // загрузка подписи
+  // загрузка подписи/печати
   $(document).on("change", ".form-file__input", function (evt) {
+    const formFileWrapper = $(this).parents(".form-file__wrapper");
+    formFileWrapper.addClass("form-file__wrapper--changing");
+
     const file = evt.target.files[0];
-
     if (file.type.startsWith("image/")) {
+      const fileObj = URL.createObjectURL(file);
       const fileName = file.name;
-      const formFileWrapper = $(this).parents(".form-file__wrapper");
-      const formFileText = formFileWrapper.find(".form-file__text");
 
-      formFileWrapper.addClass("form-file__wrapper--done");
-      formFileText.text(fileName);
+      if ($(this).hasClass("form-file__input--signature")) {
+        $("#add_image").addClass("popup--signature");
+        $("#add_image .popup__title").text("Добавление подписи");
+        $("#add_image .popup__text").text(
+          "Расположите подпись как можно ближе к границам рамки."
+        );
+        $("#add_image .popup__placeholder").html(
+          `
+          <div class="popup__wrapper popup__wrapper--signature">
+            <img class="popup__img" src="${fileObj}" alt="${fileName}" loading="lazy">
+          </div>
+          `
+        );
+      } else {
+        $("#add_image .popup__title").text("Добавление печати");
+        $("#add_image .popup__text").text(
+          "Расположите печать как можно ближе к границам рамки."
+        );
+        $("#add_image .popup__placeholder").html(
+          `
+          <div class="popup__wrapper popup__wrapper">
+            <img class="popup__img" src="${fileObj}" alt="${fileName}" loading="lazy">
+          </div>
+          `
+        );
+      }
+
+      $.fancybox.open({
+        src: "#add_image",
+      });
     }
   });
 
+  $(document).on("click", ".popup--signature .popup__submit", function (evt) {
+    evt.preventDefault();
+
+    const popup = $(this).parents(".popup");
+    const fileName = popup.find(".popup__img").prop("alt");
+    const formFileWrapper = $(".form-file__wrapper--changing");
+    const formFileText = formFileWrapper.find(".form-file__text");
+
+    formFileWrapper.addClass("form-file__wrapper--done");
+    formFileText.text(fileName);
+
+    formFileWrapper.removeClass("form-file__wrapper--changing");
+
+    $.fancybox.close({
+      src: "#add_image",
+    });
+  });
+
+  // сброс подписи/печати
   $(document).on(
     "click",
     ".form-file__wrapper--done .form-file__btn",
@@ -193,4 +241,79 @@ $(document).ready(function () {
       formFileInput.val("");
     }
   );
+
+  // input range
+  for (let e of document.querySelectorAll(".form-range")) {
+    e.style.setProperty("--value", e.value);
+    e.style.setProperty("--min", e.min == "" ? "0" : e.min);
+    e.style.setProperty("--max", e.max == "" ? "100" : e.max);
+    e.addEventListener("input", () => e.style.setProperty("--value", e.value));
+  }
+
+  // Добавить сторону
+  $(".js-add--side").click(function (evt) {
+    evt.preventDefault();
+
+    let sideCount = $("#side_list").children().length;
+
+    const sideLine = `
+    <div class="form-line form-line--flex">
+    <div class="form-item w-336">
+      <label for="job_${sideCount}" class="form-label">Должность</label>
+      <input class="form-input" type="text" name="job_${sideCount}" id="job_${sideCount}">
+    </div>
+    <div class="form-item w-336">
+      <label for="fio_${sideCount}" class="form-label">ФИО</label>
+      <input class="form-input" type="text" name="fio_2" id="fio_${sideCount}">
+    </div>
+    <div class="form-item form-file w-266">
+      <div class="form-file__wrapper">
+        <label for="file_${sideCount}" class="form-file__btn"></label>
+        <label for="file_${sideCount}" class="form-file__label">
+          <input class="form-file__input form-file__input--signature" type="file" name="file_${sideCount}" id="file_${sideCount}">
+          <span class="form-file__text">Добавить подпись</span>
+        </label>
+      </div>
+
+    </div>
+    <div class="form-item form-item--remove">
+      <button type="button" class="form-remove form-remove--side" title="Удалить"></button>
+    </div>
+  </div>
+    `;
+
+    $("#add_side").before(sideLine);
+  });
+
+  // добавить предупреждение
+  $("#alert_switch").on("change", function (evt) {
+    evt.preventDefault();
+
+    const billRowSection = $(this).parents(".bill-row__section");
+    const billRowSectionHidden = billRowSection.find(".form__wrapper--hidden");
+
+    billRowSectionHidden.toggle();
+  });
+
+  // выбор цвета
+  $("#colorBtn").click(function (evt) {
+    evt.preventDefault();
+
+    $("#colorMenu").toggleClass("form-color__menu--active");
+  });
+
+  $(".form-color__label").click(function () {
+    const newValue = $(this).prop("for");
+    $("#colorBtn .form-color__value").css({ backgroundColor: "#" + newValue });
+
+    $("#colorMenu").removeClass("form-color__menu--active");
+  });
+
+  // Закрыть попап выбора цвета по клику вне его
+  $(document).mouseup(function (evt) {
+    const colorMenu = $("#colorMenu");
+    if (!colorMenu.is(evt.target) && colorMenu.has(evt.target).length === 0) {
+      colorMenu.removeClass("form-color__menu--active");
+    }
+  });
 });
